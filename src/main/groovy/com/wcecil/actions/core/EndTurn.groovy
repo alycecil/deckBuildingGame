e66e8417923c50c721dd4358a1e1cf12c2299608
wcecil <wilcecil@gmail.com>
2015-07-30 00:01:04 -0400

    refactor

--- src/main/groovy/com/wcecil/actions/core/EndTurn.groovy
+++ src/main/groovy/com/wcecil/actions/core/EndTurn.groovy
@@ -15,24 +15,23 @@ class EndTurn extends AbstractAction {
 
 	def doAction(GameState g) {
 		Player currentPlayer  =g.currentPlayer
-		
+
 		Player nextPlayer = null
 		int index = g.players.indexOf(currentPlayer)
 		index++
 		index = index % g.players.size()
 		nextPlayer = g.players.get(index)
-		
-		g.players.each {
-			Player p ->
-			
+
+		g.players.each { Player p ->
+
 			p.money = 0
-			
+
 			CardMovementHelper.moveFullZones(p.played, p.discard, g, this)
 		}
-		
+
 		CardMovementHelper.moveFullZones(currentPlayer.hand, currentPlayer.discard, g, this)
 		GameController.doAction(g, new DrawHand(targetPlayer:currentPlayer))
-		
+
 		audit = "Player ${currentPlayer.id} ended their turn, starting the turn of Player ${nextPlayer.id}"
 	}
 

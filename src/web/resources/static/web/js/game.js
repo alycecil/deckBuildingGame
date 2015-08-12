9b829b48b7b33d7ef1c34d26b3c78f7bbd469e71
wcecil <wilcecil@gmail.com>
2015-08-11 23:05:28 -0400

    UI works with tokening system

--- src/web/resources/static/web/js/game.js
+++ src/web/resources/static/web/js/game.js
@@ -1,16 +1,69 @@
 //TODO alter to real game id
-var gameId = 1
+var gameId = null
+var token = null
+
+function showLogin() {
+	var context = {}
+	_showLogin(context)
+}
+
+function _showLogin(context){
+	getTemplateAjax('handlebar/login.handlebars', function(template) {
+	    var html = template(context);
+	    $("#gamesList").html(html);
+	});
+}
+
+function createUser(){
+
+	return false;
+}
+
+function login() {
+	var usr = $('#usr').val().trim();
+	var psw = $('#pwd').val().trim();
+	
+	if(psw==null||psw==""){
+		alert("Password may not be null");
+	}
+	var hash = CryptoJS.SHA1(psw);
+	psw = hash.toString()
+	
+	var url = '/players/token?username='+usr+'&password='+psw
+	
+	try {  
+	 	$.ajax({
+	        url: url,
+	        success: function(context) {
+	            token = context.token
+	            loadGames();
+	        },
+	        error: function(){
+	        	var context = {"error":"Unknown User"}
+	        	_showLogin(context);
+	        }
+	    });
+    } catch (e) {
+	   throw new Error(e.message);
+	}
+	return false;
+}
+
 
 function loadGames() {
-    $.ajax({
-        url: '/game/list',
-        success: function(context) {
-            getTemplateAjax('handlebar/existingGameLink.handlebars', function(template) {
-                var html = template(context);
-                $("#gamesList").html(html);
-            });
-        }
-    });
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/list?token='+token,
+	        success: function(context) {
+	            getTemplateAjax('handlebar/existingGameLink.handlebars', function(template) {
+	                var html = template(context);
+	                $("#gamesList").html(html);
+	            });
+	        }
+	    });
+    }
 }
 
 function scrollToAnnouncement() {
@@ -23,6 +76,7 @@ function scrollToAnnouncement() {
 function renderGame(context){
     console.log(context);
     getTemplateAjax('handlebar/renderGame.handlebars', function(template) {
+    	gameId=context.id
         var html = template(context);
         $("#gamesList").html(html);
         $('.endTurn').click(endTurn);
@@ -37,50 +91,72 @@ function renderGame(context){
 }
 
 function newGame() {
-    $.ajax({
-        url: '/game/new',
-        success: renderGame
-    });
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/new?token='+token,
+	        success: renderGame
+	    });
+    }
+}
+
+function getGameById(_gameId) {
+	gameId = _gameId
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/get?id='+gameId+'&token='+token,
+	        success: renderGame
+	    });
+    }
 }
 
 function getGame() {
-    $.ajax({
-        url: '/game/get?id='+gameId,
-        success: renderGame
-    });
-}
-
-function getGameById(gameId) {
-    $.ajax({
-        url: '/game/get?id='+gameId,
-        success: renderGame
-    });
+	getGameById(gameId);
 }
 
 function playAll() {
-    $.ajax({
-        url: '/game/move?id='+gameId+'&action=PlayHand',
-        success: renderGame
-    });
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/move?id='+gameId+'&action=PlayHand&token='+token,
+	        success: renderGame
+	    });
+    }
 }
 
 function buyCard() {
-    $.ajax({
-        url: '/game/move?id='+gameId+'&action=BuyCard&targetCard='+$(this).attr('card'),
-        success: renderGame
-    });
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/move?id='+gameId+'&action=BuyCard&token='+token+'&targetCard='+$(this).attr('card'),
+	        success: renderGame
+	    });
+    }
 }
 
 function playCard() {
-    $.ajax({
-        url: '/game/move?id='+gameId+'&action=PlayCard&sourceCard='+$(this).attr('card'),
-        success: renderGame
-    });
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/move?id='+gameId+'&action=PlayCard&token='+token+'&sourceCard='+$(this).attr('card'),
+	        success: renderGame
+	    });
+    }
 }
 
 function endTurn() {
-    $.ajax({
-        url: '/game/move?id='+gameId+'&action=EndTurn',
-        success: renderGame
-    });
+	if(token==null){
+		showLogin()
+	}else{
+	    $.ajax({
+	        url: '/game/move?id='+gameId+'&token='+token+'&action=EndTurn',
+	        success: renderGame
+	    });
+    }
 }

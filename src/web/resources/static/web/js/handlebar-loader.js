1e72ff9339ca746612c5260adb2d8e68618000ca
wcecil <wilcecil@gmail.com>
2015-08-13 22:51:42 -0400

    Extra Styling + handlebar partials!

--- src/web/resources/static/web/js/handlebar-loader.js
+++ src/web/resources/static/web/js/handlebar-loader.js
@@ -17,3 +17,25 @@ function getTemplateAjax(path, callback) {
         }
     });
 }
+
+
+function getPartialAjax(path, name) {
+    var source;
+    var template;
+
+    $.ajax({
+      url: path,
+        success: function(data) {
+          Handlebars.registerPartial(name, data)
+        }
+    });
+}
+
+
+function registerPartials(){
+	getPartialAjax('/resources/handlebar/partial/playerHandCard.handlebars','playerHandCard')
+	getPartialAjax('/resources/handlebar/partial/blankCard.handlebars','blankCard')
+	getPartialAjax('/resources/handlebar/partial/buyCard.handlebars','buyCard')
+	getPartialAjax('/resources/handlebar/partial/player.handlebars','player')
+	getPartialAjax('/resources/handlebar/partial/renderMulticard.handlebars','renderMulticard')
+}

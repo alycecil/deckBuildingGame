9c5b21a2e156efa96553fcad6e30753e5a8c8e6e
wcecil <wilcecil@gmail.com>
2015-08-14 00:17:01 -0400

    UX Overhaul

--- src/main/groovy/com/wcecil/webservice/controller/HealthController.java
+++ src/main/groovy/com/wcecil/webservice/controller/HealthController.java
@@ -1,5 +1,9 @@
 package com.wcecil.webservice.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,4 +25,10 @@ public class HealthController {
         return context.getMongoHost();
     }
 
+    @RequestMapping({"/", "/index", "/index.html"})
+    public String root(
+			HttpServletResponse response) throws IOException {
+    	response.sendRedirect("/resources/app.html");
+        return "forward:/resources/app.html";
+    }
 }
